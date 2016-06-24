053a5906dff362956b0cccf777f5b1e58c9295ee
magic-fish <zwy1995320@qq.com>
2016-06-24 22:59:51 +0800

    封装图表绘制操作，修改UI，封装温度数据
    1、将图表绘制操作封装成Display类，增加Display.h和Display.cpp
    2、修改了UI界面，添加了部分控件
    3、将温度数据封装成TempData类,增加TempData.h和TempData.cpp
    4.删除hello.c 和hello.h

---
+++ TempretureForecast/Display.h
@@ -0,0 +1,36 @@
+#pragma once
+#include<vector>
+#include"TempData.h"
+#define DATA_SIZE 60
+
+using namespace std;
+
+class CDisplay
+{
+private:
+	int m_maxTemp;
+	int m_minTemp;
+public:
+	CDisplay();
+	~CDisplay();
+	void Init(CRect rect);
+	//绘制坐标轴
+	void DrawCoordinate(CDC *pDC,CRect rect);
+	// 绘制温度曲线图
+	void DrawGraph(CDC *pDC);
+
+	vector<CTempData>m_tempData;     //采集数据
+	vector<CTempData>m_forecastData; //预测数据
+	CRect m_codRect;  //坐标系矩形
+	void AddData(CTempData data);    //录入数据
+	// 获得最高温度和最低温度
+	void GetMinAndMaxTemp(int minTemp, int maxTemp);
+	CPen blackPen;      // 用于创建黑色画笔   画坐标轴
+	CPen bluePen;       // 用于创建蓝色画笔   画时间线
+	CPen grayPen;       // 用于创建灰色画笔   画网格线
+	CPen redPen;        // 用于创建红色画笔   画预警线
+	CPen greenPen;      // 用于创建绿色画笔   画实际温度曲线
+	CPen brownPen;      // 用于创建褐色画笔   画预测温度曲线
+	CPen *pOldPen;      // 用于存放旧画笔   
+};
+
