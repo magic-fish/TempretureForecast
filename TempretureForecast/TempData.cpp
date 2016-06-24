af9747d4bd66a59713e12eb3047242483d71162f
wjy <locojyw@outlook.com>
2016-06-24 23:36:56 +0800

    更新温度类
    添加接口利于温度数据和时间的数据的解析，将SYSTEMTIME和double 转化为CString 利于文件和数据库存储

--- TempretureForecast/TempData.cpp
+++ TempretureForecast/TempData.cpp
@@ -10,3 +10,33 @@ CTempData::CTempData()
 CTempData::~CTempData()
 {
 }
+
+
+
+CTempData::CTempData(double temperature, SYSTEMTIME date)
+{
+	this->m_date = date;
+	this->m_tempreture = temperature;
+}
+
+CString CTempData::GetTemperature()
+{
+	CString data;
+	data.Format(_T("%lf\n"), m_tempreture);
+	return data;
+}
+CString CTempData::GetTime()
+{
+	CString data;
+	data.Format(_T("%02d:%02d:%02d.%3d\n"), m_date.wHour, m_date.wMinute, m_date.wSecond, m_date.wMilliseconds);
+	return data;
+}
+
+
+// ·µ»Ø×Ö·û´®
+CString CTempData::Tostring()
+{
+	CString data;
+	data.Format(_T("%02d:%02d:%02d.%3d  %lf\n"), m_date.wHour, m_date.wMinute, m_date.wSecond, m_date.wMilliseconds, m_tempreture);
+	return data;
+}
--- TempretureForecast/TempData.h
+++ TempretureForecast/TempData.h
@@ -1,5 +1,9 @@
 #pragma once
 //温度数据类
+/*
+ * author wjy
+ * 添加接口利于数据解析
+ */
 class CTempData
 {
 public:
@@ -9,5 +13,13 @@ public:
 	double m_tempreture;
 	// 时间
 	SYSTEMTIME m_date;
+
+	CTempData(double temperature, SYSTEMTIME date);
+	//返回温度的字符串
+	CString GetTemperature();
+	//返回时间的字符串
+	CString GetTime();
+	// 返回字符串
+	CString Tostring();
 };
 
